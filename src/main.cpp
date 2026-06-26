fab691c49701da6aa580f6b1addc73a605567a86
Fredrickstein <fredricksteintheharlot@gmail.com>
2026-06-25 23:20:46 -0400

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <raylib.h>
+
+
+int main() 
+{
+    constexpr int screenWidth = 800;
+    constexpr int screenHeight = 600;
+    
+    InitWindow(screenWidth, screenHeight, "MeteorSweeper");
+    SetTargetFPS(60);
+    
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+            ClearBackground(BLACK);
+
+
+        EndDrawing();
+    }
+    
+    CloseWindow();
+}
